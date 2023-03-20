5dab5a25fc22a214463986b07d014e011754a32c
Daniel Galan <daniel.galan@col.vueling.com>
2023-03-20 20:26:20 +0100

    [WIP] Added composable components in WelcomeScreen

--- app/src/main/java/com/madteam/sunset/welcome/ui/WelcomeScreen.kt
+++ app/src/main/java/com/madteam/sunset/welcome/ui/WelcomeScreen.kt
@@ -1,34 +1,58 @@
 package com.madteam.sunset.welcome.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.madteam.sunset.R
 import com.madteam.sunset.R.string
 import com.madteam.sunset.ui.theme.primaryBoldDisplayM
+import com.madteam.sunset.ui.theme.secondaryRegularBodyL
+import com.madteam.sunset.ui.theme.secondarySemiboldHeadLineS
 
 @Composable
 fun WelcomeScreen() {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         SunsetLogoImage()
         CustomSpacer(70.dp)
         MainTitle()
+        CustomSpacer(size = 8.dp)
+        SubTitle()
+        CustomSpacer(size = 72.dp)
+        EmailButton()
+        CustomSpacer(size = 16.dp)
+        GoogleButton()
+        CustomSpacer(size = 16.dp)
+        FacebookButton()
     }
 }
 
@@ -50,17 +74,121 @@ fun CustomSpacer(size: Dp) {
 fun MainTitle() {
     Column {
         Text(
+            modifier = Modifier.fillMaxWidth(),
             text = stringResource(string.hello),
             style = primaryBoldDisplayM
         )
         Text(
+            modifier = Modifier.fillMaxWidth(),
             text = stringResource(string.we_are_sunset),
             style = primaryBoldDisplayM
         )
     }
 }
 
-@Preview(showSystemUi = true)
+@Composable
+fun SubTitle() {
+    Text(
+        text = stringResource(string.welcome_subtitle),
+        style = secondaryRegularBodyL
+    )
+}
+
+@Composable
+fun EmailButton() {
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp)
+            .clip(RoundedCornerShape(16.dp)),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color(0xFFFFB600),
+            contentColor = Color(0xFFFFFFFF),
+        )
+    ) {
+        Text(
+            modifier = Modifier.align(Alignment.CenterVertically),
+            text = stringResource(string.btn_continue_email),
+            color = Color(0xFFFFFFFF),
+            style = secondarySemiboldHeadLineS
+        )
+    }
+}
+
+@Composable
+fun GoogleButton() {
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color(0xFFFFFFFF),
+        ),
+        contentPadding = PaddingValues(start = 24.dp),
+        shape = RoundedCornerShape(16.dp),
+        border = BorderStroke(1.dp, Color(0xFF000000))
+    ) {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .align(Alignment.CenterVertically)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_google),
+                contentDescription = "Logo Google",
+                modifier = Modifier
+                    .size(width = 27.84.dp, height = 30.dp)
+                    .align(Alignment.CenterStart)
+            )
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = stringResource(string.btn_continue_google),
+                color = Color(0xFF000000),
+                style = secondarySemiboldHeadLineS
+            )
+        }
+    }
+}
+
+@Composable
+fun FacebookButton() {
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color(0xFFFFFFFF),
+        ),
+        contentPadding = PaddingValues(start = 24.dp),
+        shape = RoundedCornerShape(16.dp),
+        border = BorderStroke(1.dp, Color(0xFF000000))
+    ) {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .align(Alignment.CenterVertically)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_facebook),
+                contentDescription = "Logo Google",
+                modifier = Modifier
+                    .size(width = 27.84.dp, height = 30.dp)
+                    .align(Alignment.CenterStart)
+            )
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = stringResource(string.btn_continue_facebook),
+                color = Color(0xFF000000),
+                style = secondarySemiboldHeadLineS
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true, device = Devices.DEFAULT)
 @Composable
 fun WelcomeScreenPrev() {
     WelcomeScreen()

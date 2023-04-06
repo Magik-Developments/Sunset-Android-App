b50dec3161e67c815073484fdbdae812b1401766
adrifernandev <adrifernandevs@gmail.com>
2023-04-06 13:34:05 +0200

    [feature] Fixed suspend fun

--- app/src/main/java/com/madteam/sunset/welcome/ui/welcome/WelcomeViewModel.kt
+++ app/src/main/java/com/madteam/sunset/welcome/ui/welcome/WelcomeViewModel.kt
@@ -5,12 +5,15 @@ package com.madteam.sunset.welcome.ui.welcome
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.material.ModalBottomSheetValue.*
+import androidx.compose.runtime.MonotonicFrameClock
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,9 +22,7 @@ class WelcomeViewModel @Inject constructor() : ViewModel() {
     private val _sheetState: MutableStateFlow<ModalBottomSheetState> = MutableStateFlow(ModalBottomSheetState(Hidden))
     val sheetState: StateFlow<ModalBottomSheetState> = _sheetState
 
-    fun expandBottomSheet() {
-        viewModelScope.launch {
-            _sheetState.value.animateTo(Expanded)
-        }
+    suspend fun expandBottomSheet() {
+        _sheetState.value.animateTo(Expanded)
     }
 }

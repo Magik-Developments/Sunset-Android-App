2d24d275f6ac0b56941262ea87dfcc78492c178c
adrifernandev <adrifernandevs@gmail.com>
2023-04-06 13:23:28 +0200

    [feature] Removed suspend fun and implemented viewModelScope

--- app/src/main/java/com/madteam/sunset/welcome/ui/welcome/WelcomeViewModel.kt
+++ app/src/main/java/com/madteam/sunset/welcome/ui/welcome/WelcomeViewModel.kt
@@ -6,9 +6,11 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.material.ModalBottomSheetValue.*
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,7 +19,9 @@ class WelcomeViewModel @Inject constructor() : ViewModel() {
     private val _sheetState: MutableStateFlow<ModalBottomSheetState> = MutableStateFlow(ModalBottomSheetState(Hidden))
     val sheetState: StateFlow<ModalBottomSheetState> = _sheetState
 
-    suspend fun expandBottomSheet() {
-        _sheetState.value.animateTo(Expanded)
+    fun expandBottomSheet() {
+        viewModelScope.launch {
+            _sheetState.value.animateTo(Expanded)
+        }
     }
 }

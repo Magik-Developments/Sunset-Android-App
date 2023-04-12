f156da807f8654f7d77b25f21301792d015f9bbf
adrifernandev <adrifernandevs@gmail.com>
2023-04-12 07:10:53 +0200

    [feature] Changed our email regex to Patters.Email_address from kotlin

--- app/src/main/java/com/madteam/sunset/welcome/ui/signin/SignInViewModel.kt
+++ app/src/main/java/com/madteam/sunset/welcome/ui/signin/SignInViewModel.kt
@@ -1,5 +1,6 @@
 package com.madteam.sunset.welcome.ui.signin
 
+import android.util.Patterns
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -9,27 +10,26 @@ import javax.inject.Inject
 @HiltViewModel
 class SignInViewModel @Inject constructor() : ViewModel() {
 
-    private val _email = MutableStateFlow("")
-    val email: StateFlow<String> = _email
+  private val _email = MutableStateFlow("")
+  val email: StateFlow<String> = _email
 
-    private val _password = MutableStateFlow("")
-    val password: StateFlow<String> = _password
+  private val _password = MutableStateFlow("")
+  val password: StateFlow<String> = _password
 
-    private val _formError = MutableStateFlow(false)
-    val formError: StateFlow<Boolean> = _formError
+  private val _formError = MutableStateFlow(false)
+  val formError: StateFlow<Boolean> = _formError
 
-    fun onValuesSignInChange(emailValue: String, passwordValue: String) {
-        _email.value = emailValue
-        _password.value = passwordValue
-        checkIfFormIsValid()
-    }
+  fun onValuesSignInChange(emailValue: String, passwordValue: String) {
+    _email.value = emailValue
+    _password.value = passwordValue
+    checkIfFormIsValid()
+  }
 
-    private fun checkIfEmailIsValid(): Boolean {
-        val emailRegex = Regex("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")
-        return emailRegex.matches(_email.value)
-    }
+  private fun checkIfEmailIsValid(): Boolean {
+    return Patterns.EMAIL_ADDRESS.matcher(_email.value).matches()
+  }
 
-    fun checkIfFormIsValid(){
-        _formError.value = (checkIfEmailIsValid() && _password.value.isNotBlank())
-    }
+  fun checkIfFormIsValid() {
+    _formError.value = (checkIfEmailIsValid() && _password.value.isNotBlank())
+  }
 }

c2d3cc6ef621d8e47490fd1586ef2640da2347dc
adriafernandezarans <adrifernandevs@gmail.com>
2023-04-30 11:55:18 +0200

    [Feat] implemented const into users collection path

--- app/src/main/java/com/madteam/sunset/repositories/DatabaseRepository.kt
+++ app/src/main/java/com/madteam/sunset/repositories/DatabaseRepository.kt
@@ -10,12 +10,12 @@ import kotlinx.coroutines.tasks.await
 import java.util.Calendar
 import javax.inject.Inject
 
+private const val USERS_COLLECTION_PATH = "users"
+
 class DatabaseRepository @Inject constructor(
     private val firebaseFirestore: FirebaseFirestore
 ) : DatabaseContract {
 
-    // TODO: Tu mindk es 24 y esta funcion solo permite crear usuarios a partir de la 26
-//    @RequiresApi(VERSION_CODES.O)
     override fun createUser(
         email: String,
         username: String,
@@ -23,7 +23,7 @@ class DatabaseRepository @Inject constructor(
     ): Flow<Resource<Unit>> = flow {
         emit(Resource.Loading())
 
-        val userDocument = firebaseFirestore.collection("users").document(username)
+        val userDocument = firebaseFirestore.collection(USERS_COLLECTION_PATH).document(username)
         val documentSnapshot = userDocument.get().await()
         if (documentSnapshot.exists()) {
             emit(Resource.Error("El usuario ya existe en la base de datos"))
@@ -38,16 +38,14 @@ class DatabaseRepository @Inject constructor(
             "creation_date" to currentDate
         )
 
-        // FIXME: No deberias tener strings hardcoded ("users") en mitad del codigo, es mala practica.
-        // Si el dia de mñn quieres cambiar el nombre de la coleccion, vas a tener que cambiar este string en mil sitios.
-        firebaseFirestore.collection("users").document(username).set(user).await()
+        firebaseFirestore.collection(USERS_COLLECTION_PATH).document(username).set(user).await()
         emit(Resource.Success(Unit))
     }.catch {
         emit(Resource.Error(it.message.toString()))
     }
 
     override fun getProfileByUsername(email: String, userProfileCallback: (UserProfile) -> Unit) {
-        firebaseFirestore.collection("users").whereEqualTo("email", email)
+        firebaseFirestore.collection(USERS_COLLECTION_PATH).whereEqualTo("email", email)
             .limit(1)
             .get()
             .addOnSuccessListener { userDocument ->

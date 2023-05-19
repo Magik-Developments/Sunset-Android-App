a9d44d49c35639bf3b1f2120cb0af07093faba27
adrifernandev <adrifernandevs@gmail.com>
2023-05-19 21:36:51 +0200

    [feature] Extracted SpotClusterInfo

--- app/src/main/java/com/madteam/sunset/ui/screens/discover/DiscoverScreen.kt
+++ app/src/main/java/com/madteam/sunset/ui/screens/discover/DiscoverScreen.kt
@@ -1,32 +1,20 @@
 package com.madteam.sunset.ui.screens.discover
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
-import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MapStyleOptions
@@ -38,10 +26,7 @@ import com.google.maps.android.compose.MapsComposeExperimentalApi
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.madteam.sunset.R
 import com.madteam.sunset.model.SpotClusterItem
-import com.madteam.sunset.ui.common.RoundedCloseIconButton
 import com.madteam.sunset.ui.common.SunsetBottomNavigation
-import com.madteam.sunset.ui.theme.primaryBoldHeadlineS
-import com.madteam.sunset.ui.theme.secondaryRegularBodyL
 import com.madteam.sunset.utils.googlemaps.MapState
 import com.madteam.sunset.utils.googlemaps.clusters.CustomClusterRenderer
 import com.madteam.sunset.utils.googlemaps.clusters.ZoneClusterManager
@@ -144,42 +129,6 @@ fun DiscoverContent(
             }
           }
         }
-      }
-    }
-  }
-}
-
-@OptIn(ExperimentalGlideComposeApi::class)
-@Composable fun SpotClusterInfo(selectedCluster: SpotClusterItem, onClose: () -> Unit) {
-  Card(
-    modifier = Modifier
-      .fillMaxWidth()
-      .height(140.dp),
-    backgroundColor = Color.White,
-    shape = RoundedCornerShape(20.dp),
-    elevation = 2.dp,
-  ) {
-    Row(modifier = Modifier.fillMaxWidth()) {
-      Box() {
-        GlideImage(
-          modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth(0.4f),
-          model = "https://t4.ftcdn.net/jpg/01/04/78/75/360_F_104787586_63vz1PkylLEfSfZ08dqTnqJqlqdq0eXx.jpg",
-          contentDescription = "Image of Sunset Spot",
-          contentScale = ContentScale.Crop
-        )
-        Box(modifier = Modifier.padding(8.dp)) {
-          RoundedCloseIconButton { onClose() }
-        }
-      }
-      Column(
-        modifier = Modifier
-          .fillMaxSize()
-          .padding(8.dp)
-      ) {
-        Text(text = selectedCluster.title, style = primaryBoldHeadlineS)
-        Text(text = "Lorem ipsum", style = secondaryRegularBodyL)
       }
     }
   }

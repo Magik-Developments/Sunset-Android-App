5fdedccf12131ddddc3185b86884a8d086ff7728
adrifernandev <adrifernandevs@gmail.com>
2023-05-19 13:57:15 +0200

    [feature] Extracted json to raw res folder

--- app/src/main/java/com/madteam/sunset/ui/screens/discover/DiscoverScreen.kt
+++ app/src/main/java/com/madteam/sunset/ui/screens/discover/DiscoverScreen.kt
@@ -1,6 +1,5 @@
 package com.madteam.sunset.ui.screens.discover
 
-import android.content.Context
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,6 +14,7 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,7 +28,6 @@ import androidx.navigation.NavController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.maps.android.compose.GoogleMap
@@ -37,13 +36,14 @@ import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.MapsComposeExperimentalApi
 import com.google.maps.android.compose.rememberCameraPositionState
+import com.madteam.sunset.R
+import com.madteam.sunset.model.SpotClusterItem
 import com.madteam.sunset.ui.common.RoundedCloseIconButton
 import com.madteam.sunset.ui.common.SunsetBottomNavigation
 import com.madteam.sunset.ui.theme.primaryBoldHeadlineS
 import com.madteam.sunset.ui.theme.secondaryRegularBodyL
 import com.madteam.sunset.utils.googlemaps.MapState
 import com.madteam.sunset.utils.googlemaps.clusters.CustomClusterRenderer
-import com.madteam.sunset.model.SpotClusterItem
 import com.madteam.sunset.utils.googlemaps.clusters.ZoneClusterManager
 import kotlinx.coroutines.launch
 
@@ -70,10 +70,7 @@ fun DiscoverScreen(
           selectedCluster = { clusterItem ->
             viewModel.selectedCluster.value = clusterItem
           },
-          calculateZoneViewCenter = viewModel::calculateZoneLatLngBounds,
-          onClusterClicked = { clusterItem ->
-            viewModel.selectedCluster.value = clusterItem
-          }
+          calculateZoneViewCenter = viewModel::calculateZoneLatLngBounds
         )
 
         if (selectedCluster != null) {
@@ -82,7 +79,7 @@ fun DiscoverScreen(
               .align(Alignment.BottomCenter)
               .padding(24.dp)
           ) {
-            TestSelectedComposable(selectedCluster!!) { viewModel.selectedCluster.value = null }
+            SpotClusterInfo(selectedCluster!!) { viewModel.selectedCluster.value = null }
           }
         }
 
@@ -91,93 +88,19 @@ fun DiscoverScreen(
   )
 }
 
-@OptIn(ExperimentalGlideComposeApi::class)
-@Composable fun TestSelectedComposable(selectedCluster: SpotClusterItem, onClose: () -> Unit) {
-  Card(
-    modifier = Modifier
-      .fillMaxWidth()
-      .height(140.dp),
-    backgroundColor = Color.White,
-    shape = RoundedCornerShape(20.dp),
-    elevation = 2.dp,
-  ) {
-    Row(modifier = Modifier.fillMaxWidth()) {
-      Box() {
-        GlideImage(
-          modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth(0.4f),
-          model = "https://t4.ftcdn.net/jpg/01/04/78/75/360_F_104787586_63vz1PkylLEfSfZ08dqTnqJqlqdq0eXx.jpg",
-          contentDescription = "",
-          contentScale = ContentScale.Crop
-        )
-        Box(modifier = Modifier.padding(8.dp)) {
-          RoundedCloseIconButton { onClose() }
-        }
-      }
-      Column(
-        modifier = Modifier
-          .fillMaxSize()
-          .padding(8.dp)
-      ) {
-        Text(text = selectedCluster.title, style = primaryBoldHeadlineS)
-        Text(text = "Lorem ipsum", style = secondaryRegularBodyL)
-      }
-    }
-  }
-}
-
 @OptIn(MapsComposeExperimentalApi::class)
 @Composable
 fun DiscoverContent(
   mapState: MapState,
   selectedCluster: (SpotClusterItem) -> Unit,
-  calculateZoneViewCenter: () -> LatLngBounds,
-  onClusterClicked: (SpotClusterItem) -> Unit
+  calculateZoneViewCenter: () -> LatLngBounds
 ) {
-
-  val styleJson = """
-    [
-        {
-            "featureType": "poi",
-            "elementType": "labels",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "road",
-            "elementType": "labels",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "water",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "color": "#CDE2F0"
-                }
-            ]
-        },
-        {
-            "featureType": "poi",
-            "elementType": "labels.icon",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        }
-    ]
-""".trimIndent()
-
   val context = LocalContext.current
+  val styleJson = remember {
+    val inputStream = context.resources.openRawResource(R.raw.map_style)
+    val json = inputStream.bufferedReader().use { it.readText() }
+    json
+  }
   val mapProperties = MapProperties(
     isMyLocationEnabled = mapState.lastKnownLocation != null,
     mapStyleOptions = MapStyleOptions(styleJson)
@@ -206,7 +129,7 @@ fun DiscoverContent(
         map.setOnMarkerClickListener(clusterManager)
         clusterManager.setOnClusterClickListener { cluster ->
           val clusterItem = cluster.items.firstOrNull()
-          clusterItem?.let { onClusterClicked(it) }
+          clusterItem?.let { selectedCluster(it) }
           true
         }
         map.setOnMapLoadedCallback {
@@ -226,6 +149,38 @@ fun DiscoverContent(
   }
 }
 
-@Composable
-fun getClusterManager(context: Context, map: GoogleMap): ZoneClusterManager =
-  ZoneClusterManager(context, map)
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable fun SpotClusterInfo(selectedCluster: SpotClusterItem, onClose: () -> Unit) {
+  Card(
+    modifier = Modifier
+      .fillMaxWidth()
+      .height(140.dp),
+    backgroundColor = Color.White,
+    shape = RoundedCornerShape(20.dp),
+    elevation = 2.dp,
+  ) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+      Box() {
+        GlideImage(
+          modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(0.4f),
+          model = "https://t4.ftcdn.net/jpg/01/04/78/75/360_F_104787586_63vz1PkylLEfSfZ08dqTnqJqlqdq0eXx.jpg",
+          contentDescription = "Image of Sunset Spot",
+          contentScale = ContentScale.Crop
+        )
+        Box(modifier = Modifier.padding(8.dp)) {
+          RoundedCloseIconButton { onClose() }
+        }
+      }
+      Column(
+        modifier = Modifier
+          .fillMaxSize()
+          .padding(8.dp)
+      ) {
+        Text(text = selectedCluster.title, style = primaryBoldHeadlineS)
+        Text(text = "Lorem ipsum", style = secondaryRegularBodyL)
+      }
+    }
+  }
+}

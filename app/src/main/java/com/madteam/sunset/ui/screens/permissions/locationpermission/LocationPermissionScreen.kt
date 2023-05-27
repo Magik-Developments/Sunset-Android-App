b490f75d85b50474f8ed278f93dcdf60c790198c
adrifernandev <adrifernandevs@gmail.com>
2023-05-27 17:48:22 +0200

    [feature] Implemented Permission location screen

--- app/src/main/java/com/madteam/sunset/ui/screens/permissions/locationpermission/LocationPermissionScreen.kt
+++ app/src/main/java/com/madteam/sunset/ui/screens/permissions/locationpermission/LocationPermissionScreen.kt
@@ -1,18 +1,24 @@
 package com.madteam.sunset.ui.screens.permissions.locationpermission
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,65 +38,88 @@ import com.madteam.sunset.ui.theme.primaryMediumHeadlineS
 fun LocationPermissionScreen(
 
 ) {
-
     LocationPermissionContent()
 }
 
 @Composable
-fun LocationPermissionContent(
-) {
+fun LocationPermissionContent() {
 
     val animComposition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.bouncing_earth))
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 16.dp)
-            .background(Color.White),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        CustomSpacer(size = 16.dp)
-        IconButton(
-            modifier = Modifier.align(alignment = Alignment.Start),
-            onClick = { /* todo */ }
-        ) {
-            RoundedCloseIconButton(onClick = {})
+    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val radius = constraints.maxHeight / 3
+            val halfWidth = constraints.maxWidth / 2
+            val centerY = constraints.maxHeight.toFloat()
+            val arcRadius = radius * 2
+            Rect(
+                (halfWidth - radius).toFloat(), centerY - radius,
+                (halfWidth + radius).toFloat(), centerY + radius
+            )
+            drawArc(
+                color = Color(0x33FFA500),
+                startAngle = 180f,
+                sweepAngle = 180f,
+                useCenter = true,
+                size = Size(arcRadius.toFloat(), arcRadius.toFloat()),
+                topLeft = Offset(halfWidth.toFloat() - radius, centerY - radius)
+            )
         }
-        CustomSpacer(size = 48.dp)
-        LottieAnimation(
-            modifier = Modifier
-                .height(250.dp)
-                .width(250.dp),
-            composition = animComposition,
-            iterations = LottieConstants.IterateForever
-        )
-        CustomSpacer(size = 80.dp)
-        Text(text = "Enable location", style = primaryBoldHeadlineL, color = Color(0xFF333333))
-        CustomSpacer(size = 24.dp)
-        Text(
-            text = "There are many beautiful sunsets\n" +
-                    "around the world...",
-            style = primaryMediumHeadlineS,
-            color = Color(0xFF999999),
-            textAlign = TextAlign.Center
-        )
-        Text(
-            text = "but we want to show you the\n" +
-                    "ones closest to you",
-            style = primaryMediumHeadlineS,
-            color = Color(0xFF999999),
-            textAlign = TextAlign.Center
-        )
+    }
+    Box(modifier = Modifier.fillMaxSize()) {
         Column(
-            verticalArrangement = Arrangement.Bottom,
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = 24.dp),
+                .padding(horizontal = 16.dp)
+                .background(Color.Transparent),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            LargeDarkButton(onClick = { /*TODO*/ }, text = R.string.enable_location)
-            CustomSpacer(size = 60.dp)
+            CustomSpacer(size = 16.dp)
+            Column(
+                horizontalAlignment = Alignment.End,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            ) {
+                RoundedCloseIconButton(onClick = {})
+            }
+            CustomSpacer(size = 48.dp)
+            LottieAnimation(
+                modifier = Modifier
+                    .height(250.dp)
+                    .width(250.dp),
+                composition = animComposition,
+                iterations = LottieConstants.IterateForever
+            )
+            CustomSpacer(size = 80.dp)
+            Text(text = "Enable location", style = primaryBoldHeadlineL, color = Color(0xFF333333))
+            CustomSpacer(size = 24.dp)
+            Text(
+                text = "There are many beautiful sunsets\n" +
+                        "around the world...",
+                style = primaryMediumHeadlineS,
+                color = Color(0xFF999999),
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = "but we want to show you the\n" +
+                        "ones closest to you",
+                style = primaryMediumHeadlineS,
+                color = Color(0xFF999999),
+                textAlign = TextAlign.Center
+            )
+            Column(
+                verticalArrangement = Arrangement.Bottom,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                LargeDarkButton(onClick = { /*TODO*/ }, text = R.string.enable_location)
+                CustomSpacer(size = 60.dp)
+            }
         }
+
     }
 }
 

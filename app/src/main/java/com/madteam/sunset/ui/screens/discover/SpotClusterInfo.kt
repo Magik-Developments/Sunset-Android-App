e75d0689a6eeb1cbf9ae9b0beec19827995c1847
adrifernandev <adrifernandevs@gmail.com>
2023-05-27 16:23:11 +0200

    [feature] Fixed ripple on spotclusteritem on discoverscreen

--- app/src/main/java/com/madteam/sunset/ui/screens/discover/SpotClusterInfo.kt
+++ app/src/main/java/com/madteam/sunset/ui/screens/discover/SpotClusterInfo.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -35,6 +36,7 @@ fun SpotClusterInfo(
         modifier = Modifier
             .fillMaxWidth()
             .height(140.dp)
+            .clip(RoundedCornerShape(20.dp))
             .clickable { onItemClicked() },
         backgroundColor = Color.White,
         shape = RoundedCornerShape(20.dp),

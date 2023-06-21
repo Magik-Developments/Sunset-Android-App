c4caf167532b97199804c0c60b47ef421357152d
adrifernandev <adrifernandevs@gmail.com>
2023-06-21 12:59:03 +0200

    [feature] Implemented AddPostScreen and ViewModel

---
+++ app/src/main/java/com/madteam/sunset/ui/screens/addpost/AddPostScreen.kt
@@ -0,0 +1,19 @@
+package com.madteam.sunset.ui.screens.addpost
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+
+@Composable
+fun AddPostScreen(
+    viewModel: AddPostViewModel = hiltViewModel(),
+    spotReference: String,
+    navController: NavController
+) {
+
+}
+
+@Composable
+fun AddPostContent() {
+
+}
---
+++ app/src/main/java/com/madteam/sunset/ui/screens/addpost/AddPostViewModel.kt
@@ -0,0 +1,10 @@
+package com.madteam.sunset.ui.screens.addpost
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AddPostViewModel @Inject constructor(
+
+) : ViewModel()

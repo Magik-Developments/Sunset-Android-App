169158be295a4201a288db8be79679e937bad67a
adrifernandev <adrifernandevs@gmail.com>
2023-09-19 22:35:43 +0200

    [feature] Implemented shimmer when list is empty

--- app/src/main/java/com/madteam/sunset/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/madteam/sunset/ui/screens/home/HomeScreen.kt
@@ -3,19 +3,26 @@ package com.madteam.sunset.ui.screens.home
 import android.Manifest
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,6 +38,7 @@ import com.madteam.sunset.ui.common.CustomSpacer
 import com.madteam.sunset.ui.common.SunsetBottomNavigation
 import com.madteam.sunset.ui.common.SunsetInfoModule
 import com.madteam.sunset.utils.getCurrentLocation
+import com.madteam.sunset.utils.shimmerBrush
 
 @Composable
 fun HomeScreen(
@@ -115,13 +123,40 @@ fun HomeContent(
                     )
                 }
             }
-            itemsIndexed(spotsList) { _, item ->
-                FeedSpotItem(
-                    spotInfo = item,
-                    userInfo = userInfo,
-                    spotLikeClick = { spotLikeClick(item.id) },
-                    onSpotClicked = { navigateTo("spot_detail_screen/spotReference=${item.id}") }
-                )
+            if (spotsList.isNotEmpty()) {
+                itemsIndexed(spotsList) { _, item ->
+                    FeedSpotItem(
+                        spotInfo = item,
+                        userInfo = userInfo,
+                        spotLikeClick = { spotLikeClick(item.id) },
+                        onSpotClicked = { navigateTo("spot_detail_screen/spotReference=${item.id}") }
+                    )
+                }
+            } else {
+                //Shimmer function when list is empty
+                item {
+                    Card(
+                        colors = CardDefaults.cardColors(containerColor = Color.Transparent),
+                        shape = RoundedCornerShape(20.dp),
+                        modifier = Modifier
+                            .size(370.dp)
+                            .background(shimmerBrush(), shape = RoundedCornerShape(20.dp))
+                            .clip(RoundedCornerShape(20.dp))
+                    ) {
+                        Column(Modifier.fillMaxSize()) {
+
+                        }
+                    }
+                }
+                item {
+                    Card(
+                        colors = CardDefaults.cardColors(containerColor = Color.Transparent),
+                        shape = RoundedCornerShape(20.dp),
+                        modifier = Modifier
+                            .size(370.dp)
+                            .background(shimmerBrush(), shape = RoundedCornerShape(20.dp))
+                    ) {}
+                }
             }
         }
         CustomSpacer(size = 24.dp)

037ed2b074f6c24df74fe9957b89667606e4be3b
adrifernandev <adrifernandevs@gmail.com>
2023-10-14 09:52:02 +0200

    [feat] Implemented SunsetPredictionScreen screen

---
+++ app/src/main/java/com/madteam/sunset/ui/screens/sunsetprediction/SunsetPredictionScreen.kt
@@ -0,0 +1,18 @@
+package com.madteam.sunset.ui.screens.sunsetprediction
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SunsetPredictionScreen(
+    navController: NavController
+) {
+
+}
+
+@Composable
+fun SunsetPredictionContent(
+
+) {
+
+}

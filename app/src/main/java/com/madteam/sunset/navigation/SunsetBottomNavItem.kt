fd932faf12c9843031dc0a63cc42530d4a536a56
adrifernandev <adrifernandevs@gmail.com>
2023-10-14 12:38:31 +0200

    [feat] Added SunsetPrediction to bottom nav

--- app/src/main/java/com/madteam/sunset/navigation/SunsetBottomNavItem.kt
+++ app/src/main/java/com/madteam/sunset/navigation/SunsetBottomNavItem.kt
@@ -4,13 +4,21 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.WbSunny
 import androidx.compose.ui.graphics.vector.ImageVector
-import com.madteam.sunset.R
 
 sealed class SunsetBottomNavItem(var title: String, var icon: ImageVector, var route: String){
 
   object Profile : SunsetBottomNavItem("Profile", Icons.Default.Person, SunsetRoutes.MyProfileScreen.route)
-  object Discover : SunsetBottomNavItem("Discover", Icons.Default.AddCircle, SunsetRoutes.DiscoverScreen.route)
+  object Discover :
+    SunsetBottomNavItem("Discover", Icons.Default.AddCircle, SunsetRoutes.DiscoverScreen.route)
+
+  object SunsetPrediction : SunsetBottomNavItem(
+    "Sunset prediction",
+    Icons.Default.WbSunny,
+    SunsetRoutes.SunsetPredictionScreen.route
+  )
+
   object Home : SunsetBottomNavItem("Home", Icons.Filled.Home, SunsetRoutes.HomeScreen.route)
 
 

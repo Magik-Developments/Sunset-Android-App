b7d6ea90efc350dc3c0496650045d1f9a6678780
adrifernandev <adrifernandevs@gmail.com>
2023-11-19 16:56:30 +0100

    [feature] Upgraded version code and name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.madteam.sunset"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "0.2"
+        versionCode = 8
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

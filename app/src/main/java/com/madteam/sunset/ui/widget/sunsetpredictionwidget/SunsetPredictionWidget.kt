fef28d2e84a4d6836ce22ec737192fac57a2e8a8
adrifernandev <adrifernandevs@gmail.com>
2023-11-20 17:35:08 +0100

    [feature] Implemented basic design of phases

---
+++ app/src/main/java/com/madteam/sunset/ui/widget/sunsetpredictionwidget/SunsetPredictionWidget.kt
@@ -0,0 +1,171 @@
+package com.madteam.sunset.ui.widget.sunsetpredictionwidget
+
+import android.content.Context
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.Image
+import androidx.glance.ImageProvider
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.appwidget.cornerRadius
+import androidx.glance.appwidget.provideContent
+import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
+import androidx.glance.layout.Column
+import androidx.glance.layout.Row
+import androidx.glance.layout.fillMaxHeight
+import androidx.glance.layout.fillMaxWidth
+import androidx.glance.layout.height
+import androidx.glance.layout.padding
+import androidx.glance.layout.size
+import androidx.glance.state.GlanceStateDefinition
+import androidx.glance.state.PreferencesGlanceStateDefinition
+import androidx.glance.text.Text
+import com.madteam.sunset.R
+
+class SunsetPredictionWidget : GlanceAppWidget() {
+
+    override val stateDefinition: GlanceStateDefinition<*> = PreferencesGlanceStateDefinition
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
+        provideContent {
+            Box(
+                modifier = GlanceModifier
+                    .fillMaxWidth()
+                    .height(225.dp)
+                    .background(Color.White)
+                    .cornerRadius(20.dp)
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Row(
+                    modifier = GlanceModifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(),
+                    verticalAlignment = Alignment.Bottom,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = GlanceModifier.defaultWeight()
+                    ) {
+                        Image(
+                            provider = ImageProvider(R.drawable.ic_sunset),
+                            contentDescription = "",
+                            modifier = GlanceModifier.size(50.dp)
+                        )
+                        Text(
+                            text = "Sunset"
+                        )
+                        Text(
+                            text = "15:34"
+                        )
+                    }
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = GlanceModifier.defaultWeight()
+                    ) {
+                        Image(
+                            provider = ImageProvider(R.drawable.ic_golden_hour),
+                            contentDescription = "",
+                            modifier = GlanceModifier.size(50.dp)
+                        )
+                        Text(
+                            text = "Golden hour"
+                        )
+                        Text(
+                            text = "15:34"
+                        )
+                    }
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = GlanceModifier.defaultWeight()
+                    ) {
+                        Image(
+                            provider = ImageProvider(R.drawable.ic_blue_hour),
+                            contentDescription = "",
+                            modifier = GlanceModifier.size(50.dp)
+                        )
+                        Text(
+                            text = "Blue hour"
+                        )
+                        Text(
+                            text = "15:34"
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+class SunsetPredictionWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = SunsetPredictionWidget()
+}
+
+/*
+                Row(
+                    modifier = GlanceModifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(),
+                    verticalAlignment = Alignment.Bottom,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = GlanceModifier.defaultWeight()
+                    ) {
+                        Image(
+                            provider = ImageProvider(R.drawable.logo_degrade),
+                            contentDescription = "",
+                            modifier = GlanceModifier.size(50.dp)
+                        )
+                        Text(
+                            text = stringResource(id = R.string.sunset)
+                        )
+                        Text(
+                            text = "15:34"
+                        )
+                    }
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = GlanceModifier.defaultWeight()
+                    ) {
+                        Image(
+                            provider = ImageProvider(R.drawable.logo_degrade),
+                            contentDescription = "",
+                            modifier = GlanceModifier.size(50.dp)
+                        )
+                        Text(
+                            text = stringResource(id = R.string.golden_hour)
+                        )
+                        Text(
+                            text = "15:34"
+                        )
+                    }
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = GlanceModifier.defaultWeight()
+                    ) {
+                        Image(
+                            provider = ImageProvider(R.drawable.logo_degrade),
+                            contentDescription = "",
+                            modifier = GlanceModifier.size(50.dp)
+                        )
+                        Text(
+                            text = stringResource(id = R.string.blue_hour)
+                        )
+                        Text(
+                            text = "15:34"
+                        )
+                    }
+                }
+ */

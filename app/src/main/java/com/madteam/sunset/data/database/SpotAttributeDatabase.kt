9d92b5f8a98cbb1d5264bdd17d7bd35cd88e35a8
adrifernandev <adrifernandevs@gmail.com>
2023-11-29 07:18:31 +0100

    [feature] Increased SpotAttributeDatabase version

--- app/src/main/java/com/madteam/sunset/data/database/SpotAttributeDatabase.kt
+++ app/src/main/java/com/madteam/sunset/data/database/SpotAttributeDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.madteam.sunset.data.database.dao.SpotAttributeDao
 import com.madteam.sunset.data.database.entities.SpotAttributeEntity
 
-@Database(entities = [SpotAttributeEntity::class], version = 1)
+@Database(entities = [SpotAttributeEntity::class], version = 2)
 abstract class SpotAttributeDatabase : RoomDatabase() {
 
     abstract fun getSpotAttributeDao(): SpotAttributeDao

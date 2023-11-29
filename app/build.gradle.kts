40f93da06560e4521d0e2da8a8463298ab1b582f
adrifernandev <adrifernandevs@gmail.com>
2023-11-29 07:51:50 +0100

    [feature] Increased versionCode and versionName for new hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.madteam.sunset"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.0"
+        versionCode = 10
+        versionName = "1.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
